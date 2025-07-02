40d921578dd9329824912cd01f8889361bacf8f2
AnastassiyaFom <a.a.fomenkova@mail.ru>
2025-07-02 21:22:45 +0300

    #6 Добавлено отображение валюты (₽) после цены

--- app/src/main/java/ru/practicum/yandex/sprint13koh29/CatalogItemViewData.kt
+++ app/src/main/java/ru/practicum/yandex/sprint13koh29/CatalogItemViewData.kt
@@ -34,7 +34,7 @@ class CatalogItemViewHolder(
             .centerCrop()
             .into(binding.image)
         binding.title.text = viewData.item.name
-        binding.price.text = "${viewData.item.price / 100}/${viewData.item.unit}"
+        binding.price.text = "${viewData.item.price / 100} ₽/${viewData.item.unit}"
 
         if (viewData.count != null) {
             binding.addToCart.visibility = View.GONE
